fb7f977fa285229d2ed51b323d903c16e286b58b
sosoalso <sosoalso@gmail.com>
2024-03-28 17:26:40 +0900

    로그메세지 조금 수정.. socket.send 예외 발생할 때 connect 하면 잘 안됨.. 그치 그게 맞지

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ import time
 import threading
 
 
-class Client():
+class Client:
     def __init__(self, ip, port):
         self.socket = None
         self.ip = ip
@@ -11,67 +11,51 @@ class Client():
         self.connected = False
         self.thread_receive = threading.Thread(target=self.receive)
 
-    def kill_thread_receive(self):
-        print("kill?")
-
-    def alive_thread_receive(self):
-        if not self.thread_receive.is_alive():
-            self.thread_receive.start()
-
     def receive(self):
-        print("receive")
         while self.connected:
             try:
                 message = self.socket.recv(1024).decode("UTF-8")
-                self.socket.send(bytes("Client wave : " + message, "UTF-8"))
+                self.socket.send(
+                    bytes("received message from server : " + message, "UTF-8"))
                 print(message)
-
-            except socket.error as e:
-                print(e)
+            except (socket.error, Exception) as e:
+                print("receive() >err> ", e)
                 self.connect()
-                # time.sleep(2)
-
-            except Exception as e:
-                print(e)
-                self.connect()
-                # time.sleep(2)
 
     def send(self, message):
-        try:
-            self.socket.send(bytes(message), "UTF-8")
-        except socket.error as e:
-            print(e)
-            # self.kill_thread_receive()
+        if self.connected:
+            try:
+                self.socket.send(bytes(message, "UTF-8"))
+            except socket.error as e:
+                print("send() >err> ", e)
 
     def connect(self):
         try:
             self.socket = socket.socket()
             self.socket.connect((self.ip, self.port))
             self.connected = True
-            print("connected")
-            self.alive_thread_receive()
+            print("connect() >msg> Connected")
+            if not self.thread_receive.is_alive():
+                self.thread_receive.start()
         except socket.error as e:
-            print(e)
+            print("connect() >err> ", e)
             self.connected = False
-            # self.kill_thread_receive()
-            print("connection lost... reconnecting")
+            print("Connection lost... reconnecting")
             while not self.connected:
                 try:
                     self.connect()
                 except socket.error as e:
                     print(e)
-                    # self.kill_thread_receive()
                     time.sleep(3)
 
 
 def run():
     while True:
-        print('run')
+        my_client.send("hah")
         time.sleep(2)
 
 
 my_client = Client("localhost", 12345)
 my_client.connect()
-
 main = threading.Thread(target=run, daemon=True)
 main.start()
